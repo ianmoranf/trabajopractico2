5421cce0369f71ddf3f5a30021e6dcd4d3b225b3
ian <ian.moran.epet2O@gmail.com>
2022-04-26 12:11:52 -0300

    trabajo practico

---
+++ java.js
@@ -0,0 +1,11 @@
+var titulo=document.createElement("h1");
+var newContent = document.createTextNode("Hola widi estoy haciendo la tarea");
+titulo.appendChild(newContent);
+document.body.appendChild(titulo)
+
+
+var parrafo=document.createElement("p");
+var contenido = document.createTextNode("El Club Atlético Huracán es una institución social y deportiva del barrio de Parque Patricios de la ciudad de Buenos Aires, Argentina, cuya principal actividad es el fútbol masculino, en la que su primer equipo participa del torneo de Primera División.");
+parrafo.appendChild(contenido);
+document.body.appendChild(parrafo)
+
